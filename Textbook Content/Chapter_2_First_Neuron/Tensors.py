3f28617daf763094218f0acc2843059084332b08
TMTien3004 <79015549+TMTien3004@users.noreply.github.com>
2023-04-22 17:29:56 -0400

    Added Chapter 3

--- Textbook Content/Chapter_2_First_Neuron/Tensors.py
+++ Textbook Content/Chapter_2_First_Neuron/Tensors.py
@@ -4,7 +4,7 @@
 The shape of the following vector is (3, 2, 4) and with 3 as the height, 2 as the row, and 4 as the column of the array.
 You can think of it as an 3D matrix since it has 3 dimensions and 3 is the z-axis, 2 is the x-axis, and 4 is the y-axis.
 """
-3Dlist = [[[ 1 , 5 , 6 , 2 ], [ 3 , 2 , 1 , 3 ]], [[ 5 , 2 , 1 , 2 ], [ 6 , 4 , 8 , 4 ]], [[ 2 , 8 , 5 , 3 ], [ 1 , 1 , 9 , 4 ]]]
+list = [[[ 1 , 5 , 6 , 2 ], [ 3 , 2 , 1 , 3 ]], [[ 5 , 2 , 1 , 2 ], [ 6 , 4 , 8 , 4 ]], [[ 2 , 8 , 5 , 3 ], [ 1 , 1 , 9 , 4 ]]]
 
 """
 A tensor object is an object that can be represented as an array.
---
+++ Textbook Content/Chapter_3_Adding_Layers/Layer_Dense.py
@@ -0,0 +1,41 @@
+#Dense Layer Class
+#---------------------------------------------------------------------------------------------------------------------------
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+
+nnfs.init()
+"""
+- If you wish to load a pre-trained model, you will initialize the parameters to whatever that pretrained model finished with.
+- When we pass data through a model from beginning to end, this is called a forward pass
+"""
+
+"""
+- np.random.randn produces a Gaussian distribution with a mean of 0 and a variance of 1, which means that it will generate 
+random numbers, positive and negative, centered at 0 and with the mean value close to 0.
+- np.zeros function takes a desired array shape as an argument and returns an array of that shape filled with zeros.
+"""
+
+class Layer_Dense:
+    # Layer initialization
+    def __init__(self, n_inputs, n_neurons):
+        # Initialize weights and biases
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons) # We'll stick with random initialization for now
+        self.biases = np.zeros((1, n_neurons))
+        pass # using pass statement as a placeholder
+
+    # Forward pass
+    def forward(self, inputs):
+        # Calculate output values from inputs, weights and biases
+        pass # using pass statement as a placeholder
+
+#Create data set
+X, y = spiral_data(samples = 100, classes = 3)
+
+# Create Dense layer with 2 input features and 3 output values
+dense1 = Layer_Dense( 2 , 3 )
+
+# Perform a forward pass of our training data through this layer
+dense1.forward(X)
+
+print (dense1.output[:5])
