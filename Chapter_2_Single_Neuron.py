4c0a5c745826d1d52194476801e0f17969433ba1
TMTien3004 <79015549+TMTien3004@users.noreply.github.com>
2023-04-22 12:51:28 -0400

    initiai commit

---
+++ Chapter_2_Single_Neuron.py
@@ -0,0 +1,11 @@
+inputs = [1, 2, 3]
+
+"""
+Each input will also have weights. The values for weights and biases are what get "trained," and they are what make
+a model actually work (or not work)
+"""
+weights = [0.2, 0.8, -0.5]
+
+"""
+Since we're
+"""
