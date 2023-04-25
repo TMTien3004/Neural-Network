e81866913a944b29fc8151008cc65a6c43fe5267
TMTien3004 <79015549+TMTien3004@users.noreply.github.com>
2023-04-25 10:57:47 -0400

    Updated nnfs package

--- Neural_Network.py
+++ Neural_Network.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 #-------------------------------------------------------------------------------------------------------------------------
-# Use this as an alternative to nnfs package
+# Use this as an alternative to downloading nnfs package
 def create_data(points, classes):
     X = np.zeros((points*classes, 2))
     y = np.zeros(points*classes, dtype='uint8')
@@ -21,6 +21,15 @@ def spiral_data(points, classes):
         r = np.linspace(0.0, 1, points)  # radius
         t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
         X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+        y[ix] = class_number
+    return X, y
+
+def vertical_data(samples, classes):
+    X = np.zeros((samples*classes, 2))
+    y = np.zeros(samples*classes, dtype='uint8')
+    for class_number in range(classes):
+        ix = range(samples*class_number, samples*(class_number+1))
+        X[ix] = np.c_[np.random.randn(samples)*.1 + (class_number)/3, np.random.randn(samples)*.1 + 0.5]
         y[ix] = class_number
     return X, y
 #-------------------------------------------------------------------------------------------------------------------------
