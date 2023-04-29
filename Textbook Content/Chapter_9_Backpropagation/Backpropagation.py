8e1c03f9c03af46204ef3b25179600cfd289c045
TMTien3004 <79015549+TMTien3004@users.noreply.github.com>
2023-04-29 16:09:01 -0400

    Updated Chapter 9

--- Textbook Content/Chapter_9_Backpropagation/Backpropagation.py
+++ Textbook Content/Chapter_9_Backpropagation/Backpropagation.py
@@ -3,67 +3,59 @@
 import math
 import numpy as np
 
-"""
-Backpropagation is a process of using the chain rule to get the resulting output functions gradients, and they are 
-passed back through the neural network, using multiplication of the gradient of subsequent functions from later 
-layers with the current one.
+# Passed in gradient from the next layer (In this case, we are going to use an array of incremental gradient values)
+deltaValues = np.array([[1., 1., 1.], [2., 2., 2.],[3., 3., 3.]])
 
-The first step is to backpropagate our gradients by calculating derivatives and partial derivatives with respect to 
-each of our parameters and inputs. To do this, we are going to use the chain rule.
-"""
+# We have 3 sets of inputs - samples
+inputs = np.array([[1, 2, 3, 2.5], [2., 5., -1., 2],[-1.5, 2.7, 3.3, -0.8]])
+
+# Create 3 sets of weights - one set for each neuron, and each set has four inputs. (Don't forget that it is transposed)
+weights = np.array([[0.2, 0.8, -0.5, 1], [0.5, -0.91, 0.26, -0.5], [-0.26, -0.27, 0.17, 0.87]]).T
+
+# One bias for each neuron
+# Biases are the row vector with a shape (1, neurons)
+biases = np.array([[2, 3, 0.5]])
+
 # Forward pass
-inputs = [1.0, -2.0, 3.0]
-weights = [-3.0, -1.0, 2.0] 
-bias = 1.0
+layer_outputs = np.dot(inputs, weights) + biases # Dense layer
+relu_outputs = np.maximum(0, layer_outputs) # ReLU Activation
 
-# Multiplying inputs by weights
-xw0 = inputs[0] * weights[0]
-xw1 = inputs[1] * weights[1]
-xw2 = inputs[2] * weights[2]
+#ReLU activation's derivative
+deltaRelu = relu_outputs.copy()
+deltaRelu[layer_outputs <= 0] = 0
 
-# Adding weighted inputs and a bias
-z = xw0 + xw1 + xw2 + bias
+# Dense Layer
+# Gradient of the neuron is simply the dot product of each weights with the input gradient.
+deltaInputs = np.dot(deltaRelu, weights.T)
 
-# ReLU activation function
-y = max (z, 0)
+# Gradient of the weight is simply the dot product of the input with the Relu activation's derivative.
+deltaWeights = np.dot(inputs.T, deltaRelu)
 
-# Backward pass
-# The derivative from the next layer
-dvalue = 1.0 # Assume that our neuron receives a gradient of 1
+# Gradient of the biases (dbiases - sum values, do this over samples (first axis), keepdims)
+deltaBiases = np.sum(deltaRelu, axis=0, keepdims=True)
 
-# Derivative of ReLU and the chain rule
-drelu_dz = dvalue * (1. if z > 0 else 0.)
-print(drelu_dz)
+#Update parameters
+weights += -0.001 * deltaWeights
+biases += -0.001 * deltaBiases
+
+print(deltaInputs)
+print(deltaBiases)
+
+# Gradient of the ReLU Activation Function
+#-------------------------------------------------------------------------------------------------------------------------
+# Example layer output
+z = np.array([[1, 2, -3, -4],[2, -7, -1, 3],[-1, 2, 5, -1]])
+dvalues = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
 
 """
-We work on the following partial derivatives:
-- drelu_dxw0 as the partial derivative of the ReLU w.r.t. the first weighed input, w0x0
-- drelu_dxw1 as the partial derivative of the ReLU w.r.t. the second weighed input, w1x1
-- drelu_dxw2 as the partial derivative of the ReLU w.r.t. the third weighed input, w2x2
-- drelu_db as the partial derivative of the ReLU with respect to the bias, b.
+To calculate the ReLU derivative, we create an array filled with zeros by using np.zeros_like. Then, we set the values
+related to the output greater than 0 is 1. 
 """
+# ReLU activation's derivative
+drelu = np.zeros_like(z)
+drelu[z > 0] = 1
+# print(drelu)
 
-# Partial derivatives of the multiplication, the chain rule
-dsum_dxw0 = 1
-dsum_dxw1 = 1
-dsum_dxw2 = 1
-drelu_db = 1
-drelu_dxw0 = drelu_dz * dsum_dxw0
-drelu_dxw1 = drelu_dz * dsum_dxw1
-drelu_dxw2 = drelu_dz * dsum_dxw2
-drelu_db = drelu_db * drelu_db
-print(drelu_dxw0, drelu_dxw1, drelu_dxw2, drelu_db)
-
-dmul_dx0 = weights[0]
-dmul_dx1 = weights[1]
-dmul_dx2 = weights[2]
-dmul_dw0 = inputs[0]
-dmul_dw1 = inputs[1]
-dmul_dw2 = inputs[2]
-drelu_dx0 = drelu_dxw0 * dmul_dx0
-drelu_dw0 = drelu_dxw0 * dmul_dw0
-drelu_dx1 = drelu_dxw1 * dmul_dx1
-drelu_dw1 = drelu_dxw1 * dmul_dw1
-drelu_dx2 = drelu_dxw2 * dmul_dx2
-drelu_dw2 = drelu_dxw2 * dmul_dw2
-print (drelu_dx0, drelu_dw0, drelu_dx1, drelu_dw1, drelu_dx2, drelu_dw2)
+# The chain rule
+drelu *= dvalues
+# print (drelu)
---
+++ Textbook Content/Chapter_9_Backpropagation/Partial_Derivative_Example.py
@@ -0,0 +1,66 @@
+# Partial Derivative Example
+#-------------------------------------------------------------------------------------------------------------------------
+"""
+Backpropagation is a process of using the chain rule to get the resulting output functions gradients, and they are 
+passed back through the neural network, using multiplication of the gradient of subsequent functions from later 
+layers with the current one.
+
+The first step is to backpropagate our gradients by calculating derivatives and partial derivatives with respect to 
+each of our parameters and inputs. To do this, we are going to use the chain rule.
+"""
+# Forward pass
+inputs = [1.0, -2.0, 3.0]
+weights = [-3.0, -1.0, 2.0] 
+bias = 1.0
+
+# Multiplying inputs by weights
+xw0 = inputs[0] * weights[0]
+xw1 = inputs[1] * weights[1]
+xw2 = inputs[2] * weights[2]
+
+# Adding weighted inputs and a bias
+z = xw0 + xw1 + xw2 + bias
+
+# ReLU activation function
+y = max (z, 0)
+
+# Backward pass
+# The derivative from the next layer
+dvalue = 1.0 # Assume that our neuron receives a gradient of 1
+
+# Derivative of ReLU and the chain rule
+drelu_dz = dvalue * (1. if z > 0 else 0.)
+print(drelu_dz)
+
+"""
+We work on the following partial derivatives:
+- drelu_dxw0 as the partial derivative of the ReLU w.r.t. the first weighed input, w0x0
+- drelu_dxw1 as the partial derivative of the ReLU w.r.t. the second weighed input, w1x1
+- drelu_dxw2 as the partial derivative of the ReLU w.r.t. the third weighed input, w2x2
+- drelu_db as the partial derivative of the ReLU with respect to the bias, b.
+"""
+
+# Partial derivatives of the multiplication, the chain rule
+dsum_dxw0 = 1
+dsum_dxw1 = 1
+dsum_dxw2 = 1
+drelu_db = 1
+drelu_dxw0 = drelu_dz * dsum_dxw0
+drelu_dxw1 = drelu_dz * dsum_dxw1
+drelu_dxw2 = drelu_dz * dsum_dxw2
+drelu_db = drelu_db * drelu_db
+print(drelu_dxw0, drelu_dxw1, drelu_dxw2, drelu_db)
+
+dmul_dx0 = weights[0]
+dmul_dx1 = weights[1]
+dmul_dx2 = weights[2]
+dmul_dw0 = inputs[0]
+dmul_dw1 = inputs[1]
+dmul_dw2 = inputs[2]
+drelu_dx0 = drelu_dxw0 * dmul_dx0
+drelu_dw0 = drelu_dxw0 * dmul_dw0
+drelu_dx1 = drelu_dxw1 * dmul_dx1
+drelu_dw1 = drelu_dxw1 * dmul_dw1
+drelu_dx2 = drelu_dxw2 * dmul_dx2
+drelu_dw2 = drelu_dxw2 * dmul_dw2
+print (drelu_dx0, drelu_dw0, drelu_dx1, drelu_dw1, drelu_dx2, drelu_dw2)
