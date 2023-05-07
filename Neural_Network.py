c2041ab917c462ab29bd0b5184a1562f89ff10fe
TMTien3004 <79015549+TMTien3004@users.noreply.github.com>
2023-05-07 09:38:54 -0400

    Updated Chapter 9 and added Adam Optimizer

--- Neural_Network.py
+++ Neural_Network.py
@@ -263,7 +263,7 @@ class Optimizer_Adagrad:
 # RMSProp Optimizer
 class Optimizer_RMSProp:
     # Initialize the default learning rate of 1.0.
-    def __init__(self, learning_rate = 1., decay = 0., epsilon = 1e-7, rho = 0.9):
+    def __init__(self, learning_rate = 0.001, decay = 0., epsilon = 1e-7, rho = 0.9):
         self.learning_rate = learning_rate
         self.current_learning_rate = learning_rate
         self.decay = decay
@@ -298,6 +298,59 @@ class Optimizer_RMSProp:
         self.iterations += 1
 
 
+# Adam (Adaptive Momentum) Optimizer
+class Optimizer_Adam:
+    # Initialize the default learning rate of 1.0.
+    def __init__(self, learning_rate = 0.001, decay = 0., epsilon = 1e-7, beta_1 = 0.9, beta_2 = 0.999):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+
+    # Call once before any parameter updates
+    def pre_update_params(self):
+        if(self.decay):
+            self.current_learning_rate = self.learning_rate * (1. / (1 + self.decay * self.iterations))
+
+    # Update paramaters
+    def update_params(self, layer):
+        # If layer does not contain cache arrays, create and fill them with zeros
+        if not hasattr(layer, 'weight_cache'):
+            layer.weight_momentums = np.zeros_like(layer.weights)
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_momentums = np.zeros_like(layer.biases)
+            layer.bias_cache = np.zeros_like(layer.biases)
+
+        # Update momentum with current gradients
+        layer.weight_momentums = self.beta_1 * layer.weight_momentums + (1 - self.beta_1) * layer.deltaWeights
+        layer.bias_momentums = self.beta_1 * layer.bias_momentums + (1 - self.beta_1) * layer.deltaBiases
+
+        # Get corrected momentum 
+        # self.iteration is 0 at first pass
+        # and we need to start with 1 here
+        weight_momentums_corrected = layer.weight_momentums / (1 - self.beta_1 ** (self.iterations + 1 ))
+        bias_momentums_corrected = layer.bias_momentums / (1 - self.beta_1 ** (self.iterations + 1 ))
+
+        # Update cache with squared current gradients
+        layer.weight_cache = self.beta_2 * layer.weight_cache + (1 - self.beta_2) * layer.deltaWeights**2
+        layer.bias_cache = self.beta_2 * layer.bias_cache + (1 - self.beta_2) * layer.deltaBiases**2
+
+        # Get corrected cache  
+        weight_cache_corrected = layer.weight_cache / (1 - self.beta_2 ** (self.iterations + 1))
+        bias_cache_corrected = layer.bias_cache / (1 - self.beta_2 ** (self.iterations + 1))
+
+        # Vanilla SGD parameter updates + normalization with square rooted cache
+        layer.weights += -self.current_learning_rate * weight_momentums_corrected / (np.sqrt(weight_cache_corrected) + self.epsilon)
+        layer.biases += -self.current_learning_rate * bias_momentums_corrected / (np.sqrt(bias_cache_corrected) + self.epsilon)
+    
+    # Call once after any parameter updates
+    def post_update_params(self):
+        self.iterations += 1
+
+
 # We set up 100 feature sets of 3 classes
 X, y = spiral_data(points=100, classes=3)
 
@@ -313,11 +366,12 @@ layer2 = Layer_Dense(64, 3)
 # Create Softmax classifier's combined loss and activation
 loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
 
-# Create optimizer object with decay rate = 0.001 and momentum = 0.9
+# Create optimizer object
 # (There are parameters that might return better results, so set decay in anyway you want and have fun experimenting!)
 # optimizer = Optimizer_SGD(decay=1e-3, momentum=0.9)
 # optimizer = Optimizer_Adagrad(decay=1e-4)
-optimizer = Optimizer_RMSProp(learning_rate = 0.02, decay = 1e-5, rho = 0.999)
+# optimizer = Optimizer_RMSProp(learning_rate = 0.02, decay = 1e-5, rho = 0.999)
+optimizer = Optimizer_Adam(learning_rate = 0.05, decay = 5e-7)
 
 # Train in loop
 for epoch in range (10001):
