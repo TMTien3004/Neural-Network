3ca5de087c55aecf769bb3c096167461b6bae797
TMTien3004 <79015549+TMTien3004@users.noreply.github.com>
2023-05-07 00:47:31 -0400

    Added RMSProp to NN

--- Neural_Network.py
+++ Neural_Network.py
@@ -260,6 +260,44 @@ class Optimizer_Adagrad:
         self.iterations += 1
 
 
+# RMSProp Optimizer
+class Optimizer_RMSProp:
+    # Initialize the default learning rate of 1.0.
+    def __init__(self, learning_rate = 1., decay = 0., epsilon = 1e-7, rho = 0.9):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon
+        self.rho = rho
+
+    # Call once before any parameter updates
+    def pre_update_params(self):
+        if(self.decay):
+            self.current_learning_rate = self.learning_rate * (1. / (1 + self.decay * self.iterations))
+
+    # Update paramaters
+    def update_params(self, layer):
+        # If layer does not contain cache arrays, create and fill them with zeros
+        if not hasattr(layer, 'weight_cache'):
+            # If there is no weight, that also means there is no bias with momentum.
+            # So we add momentum to biases as well
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+            
+        # Update cache with squared current gradients
+        layer.weight_cache = self.rho * layer.weight_cache + (1 - self.rho) * layer.deltaWeights ** 2
+        layer.bias_cache = self.rho * layer.bias_cache + (1 - self.rho) * layer.deltaBiases ** 2
+
+        # Vanilla SGD parameter updates + normalization with square rooted cache
+        layer.weights += -self.current_learning_rate * layer.deltaWeights / (np.sqrt(layer.weight_cache) + self.epsilon)
+        layer.biases += -self.current_learning_rate * layer.deltaBiases / (np.sqrt(layer.bias_cache) + self.epsilon)
+    
+    # Call once after any parameter updates
+    def post_update_params(self):
+        self.iterations += 1
+
+
 # We set up 100 feature sets of 3 classes
 X, y = spiral_data(points=100, classes=3)
 
@@ -278,7 +316,8 @@ loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
 # Create optimizer object with decay rate = 0.001 and momentum = 0.9
 # (There are parameters that might return better results, so set decay in anyway you want and have fun experimenting!)
 # optimizer = Optimizer_SGD(decay=1e-3, momentum=0.9)
-optimizer = Optimizer_Adagrad(decay=1e-4)
+# optimizer = Optimizer_Adagrad(decay=1e-4)
+optimizer = Optimizer_RMSProp(learning_rate = 0.02, decay = 1e-5, rho = 0.999)
 
 # Train in loop
 for epoch in range (10001):
